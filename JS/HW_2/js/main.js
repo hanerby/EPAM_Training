78a759ce49bda7842df1b69e3d47564a644a14bb
mori4ok <qit91@mail.ru>
2017-10-12 22:01:34 +0300

    add simple nums(not my)

--- JS/HW_2/js/main.js
+++ JS/HW_2/js/main.js
@@ -1,12 +1,12 @@
 // Find factorial //
-var n=10;
+var n=10; 
 var res = 1;
 for(var i = n;i>1;i--){
     res = n*=(i-1);
     console.log(i, res);
 };
 // 
-var n = 5;
+var n = 5; //any var
 var res = 1;
 for(var i = 0; i< n; i++){
     res *=n-1;
@@ -19,6 +19,28 @@ for(var i = 2; i <=n; i++){
     fib[i]=fib[i-2] + fib[i-1];
     console.log(i, fib);
 };
-// Simple num //
+// Simple num // not my, i honestly confesseds
+var arr = [],
+    num = [],
+    n = 100,
+    p = 2;
+for (var i = 2; i < n; i++) {
+    arr[i] = i
+}
+do {
+for (i = 2 * p; i < n; i += p) {
+    arr[i] = false;
+}
+for (i = p + 1; i < n; i++) {
+    if (arr[i]) break;
+}
+ p = i;
+} while (p * p < n);
 
-
+for(i = 0; i < n; i++){
+    if(arr[i]){
+        num.push(arr[i]);
+    };
+};
+console.log(arr);
+console.log(num);
