e4b946853aac9a780d4490ad681e6ade2e07b416
mori4ok <qit91@mail.ru>
2017-10-17 03:42:17 +0300

    create in prototype too

--- JS/HW_4/js/main.js
+++ JS/HW_4/js/main.js
@@ -1,4 +1,4 @@
-// Functional style
+/////////////////////// Functional style /////////////////////
 function auto(model) {
     var yearBld = 0;
     this.model = model;
@@ -37,4 +37,52 @@ function wagon(model){
 var lada = new sedan('lada');
 lada.setYear(1899);
 lada.setYear(2020);
-// Prototype style
+
+///////////////////// Prototype style //////////////////
+
+function auto(model) {
+    var yearBld = 0;
+    this.model = model;
+    this.made = undefined;
+    this.year = undefined;
+};
+auto.prototype.setYear = function (year) {
+    var now = new Date().getFullYear();
+    if(year<1900){
+        throw new Error('Cannot set year, it is so old')
+    }
+    if(year>now){
+        throw new Error('Incorrec date, it is a future!!!!!')
+    }
+    yearBld = year;
+};
+auto.prototype.getYearBld = function() {
+    return yearBld;
+  };
+
+function suv(model){
+    auto.apply(this, arguments);
+    this.type = 'suv';
+};
+
+function sedan(model){
+    auto.apply(this, arguments);
+    this.type = 'sedan';
+};
+
+function wagon(model){
+    auto.apply(this, arguments);
+    this.type = 'wagon';
+};
+
+suv.prototype = Object.create(auto.prototype);
+
+sedan.prototype = Object.create(auto.prototype);
+
+wagon.prototype = Object.create(auto.prototype);
+
+var lada = new sedan('lada');
+lada.setYear(1899);
+lada.setYear(2020);
+lada.setYear(2016);
+lada.getYearBld();
