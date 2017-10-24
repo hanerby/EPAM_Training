6425cca07f1d1368ad2214240b6d295fc4449cb1
mori4ok <qit91@mail.ru>
2017-10-24 13:51:39 +0300

    cleangit add .

--- JS/HW_6/js/models/datapicker.js
+++ JS/HW_6/js/models/datapicker.js
@@ -10,12 +10,15 @@ var DataPicker = function() {
       this.todaysDate= new Date(),
       this.monthArr = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 };
+
 DataPicker.prototype.monthsAsString = function (monthIndex) {
     return this.monthArr[monthIndex];
 };
+
 DataPicker.prototype.clearCalendar = function(){
    return  this.month.innerHTML = '';
 };
+
 DataPicker.prototype.createListenersBtn = function(next, prev){
     var self = this;
     next.addEventListener('click', function () {
@@ -31,12 +34,6 @@ DataPicker.prototype.createListenersBtn = function(next, prev){
         self.createMonth();
     });
 };
-
-
-// DataPicker.prototype.init =  function () {
-//     this.createMonth();
-//     this.createListenersBtn();
-// };
 
 DataPicker.prototype.createDay = function (num, day) {
     var newDay = document.createElement('div')
@@ -71,47 +68,6 @@ DataPicker.prototype.createMonth = function () {
     this.date.setMonth(this.date.getMonth() - 1);
     this.label.innerHTML = this.monthsAsString(this.date.getMonth()) + ' ' + this.date.getFullYear();
 };
-
-
-// init: function () {
-//     this.createMonth()
-//     this.createListeners()
-// },
-//   // Create the days in month table
-//   createDay: function (num, day) {
-//     var newDay = document.createElement('div')
-//     var dateEl = document.createElement('span')
-//     dateEl.className = 'date__num';
-//     dateEl.innerHTML = num;
-//     newDay.className = 'cal__date';
-
-//     if (num === 1) {
-//       var offset = ((day ) * 14.28)
-//       if (offset > 0) {
-//         newDay.style.marginLeft = offset + '%'
-//       }
-//     };
-
-//     if (this.date.toString() === this.todaysDate.toString()) {
-//       newDay.classList.add('cal__date_today')
-//     }
-
-//     newDay.appendChild(dateEl)
-//     this.month.appendChild(newDay)
-//   },
-//   // Create month table
-//   createMonth: function () {
-//     this.date.setDate(1);
-//     var currentMonth = this.date.getMonth();
-//     while (this.date.getMonth() === currentMonth) {
-//       this.createDay(this.date.getDate(), this.date.getDay());
-//       this.date.setDate(this.date.getDate() + 1);
-//     };
-//     this.date.setDate(1);
-//     this.date.setMonth(this.date.getMonth() - 1);
-//     this.label.innerHTML = this.monthsAsString(this.date.getMonth()) + ' ' + this.date.getFullYear();
-//   },
-// }
 
 
 
